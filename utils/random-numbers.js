2b4d9334171b8d3e403f10e2ac6aded2b51964df
David Delgado <delgadoch.david@gmail.com>
2021-08-20 01:18:56 -0400

    Create .env.example

---
+++ utils/random-numbers.js
@@ -0,0 +1,15 @@
+const between = (min, max, length) => {
+  const array = [];
+
+  while (array.length < length) {
+    const number = Math.floor(Math.random() * (max - min + 1) + min);
+
+    if (array.indexOf(number) !== -1) array.push(number);
+  }
+
+  array.sort();
+
+  return array;
+};
+
+module.exports = between;
